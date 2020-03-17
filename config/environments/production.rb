c6ac1ff3c0f1268c5fe343fbc687454b9e8e4435
EC2 Default User <ec2-user@ip-172-31-46-15.ap-northeast-1.compute.internal>
2020-03-17 08:44:44 +0000

    Add account activation5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'glacial-tor-95809.herokuapp.com'
+  host = 'https://glacial-tor-95809.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app164325020@heroku.com'],
-    :password       => ENV['lvwfqcvs6427'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
