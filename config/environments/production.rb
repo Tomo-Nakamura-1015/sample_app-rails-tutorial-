53628e1e574579148cf3ecd6602761062f403c3c
EC2 Default User <ec2-user@ip-172-31-46-15.ap-northeast-1.compute.internal>
2020-03-17 08:17:42 +0000

    Add account activation4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app164325020@heroku.com'],
+    :password       => ENV['lvwfqcvs6427'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
